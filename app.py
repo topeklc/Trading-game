0c232c194c64f184839b4e8b41e12a172be0c638
topek <alienworldspl@gmail.com>
2021-10-28 22:02:07 +0200

    another...

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, render_template, redirect, url_for, session
 from main import *
 from forms import ActionForm
 
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 portfolio = Portfolio(0)
 game = Game(2017)
-
+user = ''
 
 @app.route('/')
 def mainpage():
@@ -16,14 +16,18 @@ def mainpage():
 @app.route('/', methods=['POST', 'GET'])
 def start_game():
     if request.method == 'POST':
+        global user
+        user = request.form['user']
+        session['user'] = user
         return redirect(url_for('started'))
 
 
 @app.route('/started', methods=['POST', 'GET'])
 def game_start():
-    global portfolio
+    global portfolio, user
     starting_cash = request.form['smoney']
     starting_year = request.form['year']
+    user = request.form['user']
     game = Game(starting_year)
     portfolio = Portfolio(starting_cash)
     now = game.current_day
@@ -41,7 +45,7 @@ def game_start():
     print(assets_dict)
     zipped = zip(assets_dict, forms)
     return render_template('started_game.html', cash=portfolio.cash,
-                           starting_year=starting_year, now=now, zipped=zipped, assets_dict=assets_dict, weekday=weekday)
+                           starting_year=starting_year, now=now, zipped=zipped, assets_dict=assets_dict, weekday=weekday, user=user)
 
 
 @app.route('/started', methods=['POST', 'GET'])
@@ -52,12 +56,12 @@ def next_day():
 
 @app.route('/day', methods=['POST', 'GET'])
 def another_day():
-    global game
+    global game, user, portfolio
     game.next_day()
-    portfolioo = portfolio
+    portfolio_state = portfolio
     now = game.current_day
     weekday = game.current_weekday
-    portfolio_statement = portfolioo.asset_amounts
+    portfolio_statement = portfolio_state.asset_amounts
     assets_dict = {}
     forms = []
     for asset in assets_list:
@@ -68,26 +72,25 @@ def another_day():
         except KeyError:
             pass
     zipped = zip(assets_dict, forms)
-    return render_template('started_game.html', cash=portfolioo.cash,
-                            now=now, zipped=zipped, assets_dict=assets_dict, portfolio=portfolio_statement, weekday=weekday)
+    return render_template('started_game.html', cash=portfolio.cash,
+                            now=now, zipped=zipped, assets_dict=assets_dict, portfolio=portfolio_statement, weekday=weekday, user=user)
 
 
 @app.route('/up', methods=['POST', 'GET'])
 def up_portfolio():
-    global game
-    portfolioo = portfolio
+    global game, user, portfolio
     now = game.current_day
     weekday = game.current_weekday
-    portfolio_statement = portfolioo.asset_amounts
+    portfolio_statement = portfolio.asset_amounts
     amount = 0
     for i, j in enumerate(request.args):
         if i == 0:
             amount = float(request.args.get(j))
         if i == 1:
             if j.split('-')[1] == 'buy':
-                portfolioo.buy_asset(amount, globals()[j.split('-')[0].lower()], game)
+                portfolio.buy_asset(amount, globals()[j.split('-')[0].lower()], game)
             elif j.split('-')[1] == 'sell':
-                portfolioo.sell_asset(amount, globals()[j.split('-')[0].lower()], game)
+                portfolio.sell_asset(amount, globals()[j.split('-')[0].lower()], game)
 
     assets_dict = {}
     forms = []
@@ -99,8 +102,8 @@ def up_portfolio():
         except KeyError:
             pass
     zipped = zip(assets_dict, forms)
-    return render_template('started_game.html', cash=portfolioo.cash,
-                            now=now, zipped=zipped, assets_dict=assets_dict, portfolio=portfolio_statement, weekday=weekday)
+    return render_template('started_game.html', cash=portfolio.cash,
+                            now=now, zipped=zipped, assets_dict=assets_dict, portfolio=portfolio_statement, weekday=weekday, user=user)
 
 
 if __name__ == '__main__':
